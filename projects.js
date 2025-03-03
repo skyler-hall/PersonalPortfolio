7c225d2c0d4202d70df160dd11743cfb86b7b69b
Roseline <rdami007@fiu.edu>
2025-03-03 01:26:59 -0500

    javascript functionality (minus sending messages)

---
+++ projects.js
@@ -0,0 +1,21 @@
+export const projectsInfo = [
+    {
+        projectName: 'Some project',
+        projectImage: 'https://www.economist.com/cdn-cgi/image/width=1424,quality=80,format=auto/sites/default/files/images/2015/09/blogs/economist-explains/code2.png',
+    },
+    {
+        projectName: 'Some project',
+        projectImage: 'https://images.prismic.io/vaultinum/0458a9f1-e149-4037-b9aa-aa4b4fb53c25_propriete-intellectuelle-code-source-protection-compressed.jpg?auto=compress,format&rect=0,0,2400,981&w=2400&h=981',
+    },
+    {
+        projectName: 'Some project',
+        projectImage: 'https://upload.wikimedia.org/wikipedia/commons/b/b5/International_Morse_Code.svg',
+    },{
+        projectName: 'Some project',
+        projectImage: 'https://media.istockphoto.com/id/1510696458/vector/vector-digital-green-background-of-streaming-binary-code-matrix-background-with-numbers-1-0.jpg?s=612x612&w=0&k=20&c=TTkG2rwjYzXCTVzLDcX59UjPjKD0AJSWE2BnPh7s8X4=',
+    },
+    {
+        projectName: 'Some project',
+        projectImage: 'https://miro.medium.com/v2/resize:fit:1087/1*37nudw5YFzaQOo3RYcKrZA.png',
+    }
+]
